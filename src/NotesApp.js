012f70d0e4cb6dae62a33390eeff3036170990d1
srikar.ch <srikar.0896@gmail.com>
2020-01-16 15:37:43 +0530

    added sync for inner observables

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useCallback } from "react";
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
@@ -6,6 +6,7 @@ import "prismjs/components/prism-javascript";
 import { fromEvent } from "rxjs";
 import {
   map,
+  forkJoin,
   startWith,
   debounceTime,
   distinctUntilChanged,
@@ -20,6 +21,7 @@ export default props => {
   const [title, setTitle] = useState("");
   const [notes, setNotes] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [activeVersion, setActiveVersion] = useState(null);
 
   function reducer(state, action) {
     switch (action.type) {
@@ -30,17 +32,24 @@ export default props => {
     }
   }
 
-  const [versions, versionsDispatcher] = useReducer(reducer, []);
+  function localReducer(state, action) {
+    switch (action.type) {
+      case "register":
+        console.log(state, action.data);
+        return [...state, action.data];
+      default:
+        throw new Error();
+    }
+  }
 
-  const saveNotes = notes => {
-    return fetch({
-      endpoint: `/save`,
-      response: {
-        id: versions.length + 1,
-        content: notes
-      }
+  const [versions, versionsDispatcher] = useReducer(reducer, []);
+  const [localVersions, localVersionDispatcher] = useReducer(localReducer, []);
+
+  const saveNotes = (payload, versionId) =>
+    fetch({
+      endpoint: `/save?v=${versionId}`,
+      response: payload
     });
-  };
 
   useEffect(() => {
     const titleText$ = fromEvent(
@@ -52,30 +61,56 @@ export default props => {
         setIsLoading(true);
         return notes;
       }),
-      startWith(""),
-      debounceTime(400),
-      distinctUntilChanged()
+      // startWith(""),
+      debounceTime(2000),
+      distinctUntilChanged(),
+      map(queryString => {
+        console.log("REGISTER", queryString, localVersions);
+        localVersionDispatcher({
+          type: "register",
+          data: {
+            id: localVersions.length + 1,
+            content: queryString
+          }
+        });
+        return queryString;
+      })
     );
 
-    console.log("kleup,", versions);
     const result$ = titleText$
-      .pipe(concatMap(queryString => saveNotes(queryString)))
+      .pipe(
+        concatMap(queryString =>
+          saveNotes(
+            {
+              id: localVersions.length + 1,
+              content: queryString
+            },
+            localVersions.length + 1
+          )
+        ),
+        distinctUntilChanged()
+      )
       .subscribe(response => {
         versionsDispatcher({
           type: "register",
           data: response
         });
-
+        console.log("RES ID", response.id);
+        setActiveVersion(response.id);
         setIsLoading(false);
       });
     // return () => result$.unsubscribe();
-  }, []);
+  }, [localVersions]);
 
-  console.log(versions);
+  console.log("LOCAL VERSIONS", localVersions);
+
+  const handleTitleChange = text => {
+    setTitle(text);
+  };
 
   return (
-    <div>
-      <div>
+    <div style={{ display: "flex" }}>
+      <div style={{ flexBasis: "80%" }}>
         <div style={{ display: "flex" }}>
           <span className="app-section_title">Notes</span>
           <div>{isLoading && "Saving..."}</div>
@@ -84,7 +119,7 @@ export default props => {
           <Editor
             textareaId="editor-notes-title"
             value={title}
-            onValueChange={value => setTitle(value)}
+            onValueChange={value => handleTitleChange(value)}
             highlight={code => highlight(code, languages.js)}
             padding={10}
             placeholder="Add a title"
@@ -124,11 +159,32 @@ export default props => {
       </div>
       <div>
         <div>
-          <h4 className="app-section_title">Notes</h4>
+          <div>
+            <h4 className="app-section_title">Local Versions</h4>
+          </div>
+          {localVersions.map((version, index) => (
+            <p
+              key={index}
+              style={{ color: version.id === activeVersion ? "red" : "black" }}
+            >
+              {version.id}
+            </p>
+          ))}
         </div>
-        {versions.map((versionContent, index) => (
-          <p key={index}>{index}</p>
-        ))}
+        <div>
+          <div>
+            <h4 className="app-section_title">Backend Versions</h4>
+          </div>
+          Active - {activeVersion}
+          {versions.map((version, index) => (
+            <p
+              key={index}
+              style={{ color: version.id === activeVersion ? "red" : "black" }}
+            >
+              {version.id}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
